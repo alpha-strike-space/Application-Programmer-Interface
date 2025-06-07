6d4443eea01663bc329dadbbc7753246db7e87d1
kanderson <81449406+kandrsn99@users.noreply.github.com>
2025-06-07 00:58:48 -0500

    Update Routes.h

--- Routes.h
+++ Routes.h
@@ -3,6 +3,6 @@
 # Functions for CROW_ROUTE and CROW_WEBSOCKET_ROUTE
 void setupRoutes(crow::SimpleApp& app);
 void setupWebSocket(crow::SimpleApp& app);
-# Extern to make global variables for the websocket.
+// Extern to make global variables for the websocket.
 extern std::vector<crow::websocket::connection*> ws_connections;
 extern std::mutex ws_mutex;
