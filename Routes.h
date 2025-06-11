5450523c18af1e3dfef0562ae73587612d344ad8
kanderson <81449406+kandrsn99@users.noreply.github.com>
2025-06-11 14:43:46 -0500

    Update Routes.h

--- Routes.h
+++ Routes.h
@@ -3,7 +3,8 @@
 // Functions for CROW_ROUTE and CROW_WEBSOCKET_ROUTE
 void setupRoutes(crow::SimpleApp& app);
 void setupWebSocket(crow::SimpleApp& app);
-
+// Don't violate the one rule definition.
+std::string get_pool_connection_string();
 // Extern to make global variables for the websocket.
 extern std::vector<crow::websocket::connection*> ws_connections;
 extern std::mutex ws_mutex;
