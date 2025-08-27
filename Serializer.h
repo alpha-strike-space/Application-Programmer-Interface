7ec844cc136da2333ef69ec60d466d1a2887c267
kanderson <81449406+kandrsn99@users.noreply.github.com>
2025-08-26 19:14:49 -0500

    Rename formatting functions for consistency
    Standardized naming convention to reflect actual file.

--- Serializer.h
+++ Serializer.h
@@ -4,7 +4,7 @@
 
 nlohmann::ordered_json build_incident_json(const pqxx::result& res);
 nlohmann::ordered_json build_system_json(const pqxx::result& res);
-nlohmann::ordered_json formatName(const pqxx::result& resName);
-nlohmann::ordered_json formatTopKillers(const pqxx::result& resKillers);
-nlohmann::ordered_json formatTopVictims(const pqxx::result& resVictims);
-nlohmann::ordered_json formatSystems(const pqxx::result& resSystems);
+nlohmann::ordered_json format_name(const pqxx::result& resName);
+nlohmann::ordered_json format_top_killers(const pqxx::result& resKillers);
+nlohmann::ordered_json format_top_victims(const pqxx::result& resVictims);
+nlohmann::ordered_json format_systems(const pqxx::result& resSystems);
